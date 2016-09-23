cca5d5b90d3cf954759943a803404406c39c0704
Goutam Venkatramanan <goutamvenkat@gmail.com>
2016-09-23 18:39:42 -0400

    Changed DB architecture

--- ClassMoodApp/Models/DBModels.py
+++ ClassMoodApp/Models/DBModels.py
@@ -3,50 +3,84 @@ from sqlalchemy import PickleType
 import sys
 import hashlib
 import uuid
+from datetime import datetime, timedelta
 
 db = SQLAlchemy()
 student_type, professor_type = 'STUDENT', 'PROFESSOR'
+
+class UserType(db.Model):
+    __tablename__ = 'UserType'
+    id = db.Column(db.BigInteger(), primary_key=True)
+    name = db.Column(db.String(400))
+    can_add_class = db.Column(db.Boolean())
+    can_list_classes = db.Column(db.Boolean())
+    user_relationship = db.relationship('User', backref='user_type', uselist=False, lazy='dynamic')
+    def __init__(self, name, can_add_class, can_list_classes):
+        self.id = uuid.uuid4().int
+        self.name = name
+        self.can_add_class = can_add_class
+        self.can_list_classes = can_list_classes
+
 class User(db.Model):
-    email = db.Column(db.String(400), primary_key=True)
-    user_type = db.Column(db.String(20), unique=True)
-    def __init__(self, email, user_type):
-        self.email = email
-        self.user_type = user_type
+    __tablename__ = 'User'
+    id = db.Column(db.BigInteger(), 
+                    db.ForeignKey('Lecture.professor_id'), 
+                    db.ForeignKey('Sessions.user_id'), 
+                    db.ForeignKey('Authentication.user_id'),
+                    db.ForeignKey('Roster.student_id'),
+                    primary_key=True)
+    first_name = db.Column(db.String(40))
+    last_name = db.Column(db.String(40))
+    email = db.Column(db.String(400), unique=True)
+    professor = db.relationship('Lecture')
+    sessions = db.relationship('Sessions')
+    auth = db.relationship('Authentication')
 
-class Authentication(db.Model):
-    email = db.Column(db.String(400), primary_key=True)
-    password = db.Column(db.String(sys.maxint))
-    salt = uuid.uuid4().hex
-    def __init__(self, email, password):
+    def __init__(self, first_name, last_name, email):
+        self.id = uuid.uuid4().int
+        self.first_name = first_name
+        self.last_name = last_name
         self.email = email
-        self.password = hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
-
-class Student(db.Model):
-    email = db.Column(db.String(400), primary_key=True)
-    lectures = db.Column(PickleType())
-    def __init__(self, email, lectures):
-        self.email = email
-        self.lectures = lectures
-
-class Professor(db.Model):
-    email = db.Column(db.String(400), primary_key=True)
-    lectures = db.Column(PickleType())
-    def __init__(self, email, lectures):
-        self.email = email
-        self.lectures = lectures
 
 class Lecture(db.Model):
-    name = db.Column(db.String(20), primary_key=True)
-    student_roster = db.Column(PickleType())
-    lecturer = db.Column(db.String(400))
-    def __init__(self, name, student_roster, lecturer):
+    __tablename__ = 'Lecture'
+    id = db.Column(db.BigInteger(), db.ForeignKey('Roster.lecture_id'), primary_key=True)
+    name = db.Column(db.String(40), unique=True)
+    description = db.Column(db.String(400))
+    professor_id = db.Column(db.BigInteger())
+    def __init__(self, name, description, professor_id):
+        self.id = uuid.uuid4().int
         self.name = name
-        self.student_roster = student_roster
-        self.lecturer = lecturer
+        self.professor_id = professor_id
 
-class UserSession(db.Model):
-    email = db.Column(db.String(400), primary_key=True)
-    token = db.Column(db.Integer())
-    def __init__(self, email, token_id):
-        self.email = email
+class Authentication(db.Model):
+    __tablename__ = 'Authentication'
+    user_id = db.Column(db.BigInteger(), primary_key=True)
+    password = db.Column(db.String(sys.maxint))
+    salt = uuid.uuid4().hex
+    def __init__(self, user_id, password):
+        self.user_id = user_id
+        self.password = '{}:{}'.format(hashlib.sha256(salt.encode() + password.encode()).hexdigest(), salt)
+
+class Sessions(db.Model):
+    __tablename__ = 'Sessions'
+    id = db.Column(db.BigInteger(), primary_key=True)
+    user_id = db.Column(db.BigInteger(), unique=True)
+    token = db.Column(db.BigInteger())
+    created_at = db.Column(db.DateTime())
+    expires_at = db.Column(db.DateTime())
+    def __init__(self, user_id, token):
         self.token = token
+        self.id = uuid.uuid4().int
+        self.user_id = user_id
+        self.created_at = datetime.now()
+        self.expires_at = self.created_at + timedelta(hours=2)
+
+class Roster(db.Model):
+    __tablename__ = 'Roster'
+    student_id = db.Column(db.BigInteger(), primary_key=True)
+    lecture_id = db.Column(db.BigInteger(), primary_key=True)
+    def __init__(self, student_id, lecture_id):
+        self.student_id = student_id
+        self.lecture_id = lecture_id
+    
