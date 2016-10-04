317ce24bbe94b48d9d88de66035588e3499f6bee
nvohra3 <nvohra@gatech.edu>
2016-10-03 21:45:25 -0400

    Database and Model Architecture
    Added DB models (Lecture, Gauge, and AnonymousQuestion) and touched up code in other parts of DBModels.py + __init__.py

--- ClassMoodApp/Models/DBModels.py
+++ ClassMoodApp/Models/DBModels.py
@@ -1,19 +1,19 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import PickleType
 import sys
 import hashlib
 import uuid
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, DateTime, Float, BigInteger
+from sqlalchemy.orm import relationship
 from datetime import datetime, timedelta
 
 db = SQLAlchemy()
-student_type, professor_type = 'STUDENT', 'PROFESSOR'
 
 class UserType(db.Model):
     __tablename__ = 'UserTypes'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(400))
-    can_add_class = db.Column(db.Boolean())
-    can_list_classes = db.Column(db.Boolean())
+    id = Column(Integer, primary_key=True)
+    name = Column(String(400))
+    can_add_class = Column(Boolean())
+    can_list_classes = Column(Boolean())
     def __init__(self, name, can_add_class, can_list_classes):
         self.name = name
         self.can_add_class = can_add_class
@@ -21,12 +21,12 @@ class UserType(db.Model):
 
 class User(db.Model):
     __tablename__ = 'Users'
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(40))
-    last_name = db.Column(db.String(40))
-    email = db.Column(db.String(400), unique=True)
-    user_type = db.Column(db.Integer, db.ForeignKey('UserTypes.id'))
-    user_type_rel = db.relationship('UserType')
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(40))
+    last_name = Column(String(40))
+    email = Column(String(400), unique=True)
+    user_type = Column(Integer, ForeignKey('UserTypes.id'))
+    user_type_rel = relationship('UserType')
     def __init__(self, first_name, last_name, email, user_type):
         self.first_name = first_name
         self.last_name = last_name
@@ -35,11 +35,11 @@ class User(db.Model):
 
 class Class(db.Model):
     __tablename__ = 'Classes'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(40), unique=True)
-    description = db.Column(db.String(400))
-    professor_id = db.Column(db.Integer, db.ForeignKey('Users.id'))
-    user_relation = db.relationship('User')
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40), unique=True)
+    description = Column(String(400))
+    professor_id = Column(Integer, ForeignKey('Users.id'))
+    user_relation = relationship('User')
     def __init__(self, name, description, professor_id):
         self.name = name
         self.description = description
@@ -47,9 +47,9 @@ class Class(db.Model):
 
 class Authentication(db.Model):
     __tablename__ = 'Authentication'
-    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'), primary_key=True)
-    password = db.Column(db.String(sys.maxint))
-    user_relation = db.relationship('User')
+    user_id = Column(Integer, ForeignKey('Users.id'), primary_key=True)
+    password = Column(String(sys.maxint))
+    user_relation = relationship('User')
     def __init__(self, user_id, password):
         self.user_id = user_id
         salt = uuid.uuid4().hex
@@ -57,12 +57,12 @@ class Authentication(db.Model):
 
 class Session(db.Model):
     __tablename__ = 'Sessions'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'), unique=True)
-    token = db.Column(db.BigInteger())
-    created_at = db.Column(db.DateTime())
-    expires_at = db.Column(db.DateTime())
-    user_relation = db.relationship('User')
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('Users.id'), unique=True)
+    token = Column(BigInteger())
+    created_at = Column(DateTime())
+    expires_at = Column(DateTime())
+    user_relation = relationship('User')
     def __init__(self, user_id, token):
         self.token = token
         self.user_id = user_id
@@ -73,25 +73,65 @@ class Session(db.Model):
 
 class ClassMember(db.Model):
     __tablename__ = 'ClassMembers'
-    student_id = db.Column(db.Integer, db.ForeignKey('Users.id'), primary_key=True)
-    class_id = db.Column(db.Integer, db.ForeignKey('Classes.id'), primary_key=True)
-    user_relation = db.relationship('User')
-    lecture_relation = db.relationship('Class')
+    student_id = Column(Integer, ForeignKey('Users.id'), primary_key=True)
+    class_id = Column(Integer, ForeignKey('Classes.id'), primary_key=True)
+    user_relation = relationship('User')
+    lecture_relation = relationship('Class')
     def __init__(self, student_id, class_id):
         self.student_id = student_id
         self.class_id = class_id
 
-def db_add(obj):
+class Lecture(db.Model):
+    __tablename__ = 'Lectures'
+    id = Column(Integer, primary_key=True)
+    class_id = Column(Integer, ForeignKey('Classes.id'))
+    is_live = Column(Boolean, nullable=False)
+    pace_total = Column(Float, nullable=False)
+    depth_total = Column(Float, nullable=False)
+    num_students = Column(Integer, nullable=False)
+    def __init__(self, class_id):
+        self.class_id = class_id
+        self.is_live = False
+        self.pace_total = 0
+        self.depth_total = 0
+        self.num_students = 0
+
+class Gauge(db.Model):
+    __tablename__ = 'Gauges'
+    id = Column(Integer, primary_key=True)
+    lecture_id = Column(Integer, ForeignKey('Lectures.id'))
+    depth = Column(Float, nullable=False)
+    pace = Column(Float, nullable=False)
+    student_id = Column(Integer, ForeignKey('Users.id'))
+    def __init__(self, lecture_id, student_id):
+        self.lecture_id = lecture_id
+        self.depth = 0
+        self.pace = 0
+        self.student_id = student_id
+
+class AnonymousQuestion(db.Model):
+    __tablename__ = 'AnonymousQuestions'
+    id = Column(Integer, primary_key=True)
+    lecture_id = Column(Integer, ForeignKey('Lectures.id'))
+    text = Column(String(1000), nullable=False)
+    student_id = Column(Integer, ForeignKey('Users.id'))
+    def __init__(self, lecture_id, text, student_id):
+        self.lecture_id = lecture_id
+        self.text = text
+        self.student_id = student_id
+
+def db_add(*obj):
     if obj:
-        db.session.add(obj)
+        for entry in obj:
+            db.session.add(entry)
         db.session.commit()
         return True
     return False
 
-def db_rem(obj):
+def db_rem(*obj):
     if obj:
-        db.session.delete(obj)
+        for entry in obj:
+            db.session.delete(obj)
         db.session.commit()
         return True
     return False
-
--- ClassMoodApp/__init__.py
+++ ClassMoodApp/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
-from ClassMoodApp.Models.DBModels import db
-from ClassMoodApp.Models.DBModels import UserType, User, Authentication, Class, ClassMember
+from ClassMoodApp.Models.DBModels import *
 from ClassMoodApp.Config.AppConfig import DevelopmentConfig
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 db.init_app(app)
+session = db.session
+
+STUDENT_TYPE = "STUDENT"
+PROFESSOR_TYPE = "PROFESSOR"
 
 # If we are in testing mode, add some test data
 if app.config.get('TESTING', False):
@@ -15,42 +18,64 @@ if app.config.get('TESTING', False):
         db.create_all()
 
         # Create the user types
-        studentType = UserType("STUDENT", False, True)
-        db.session.add(studentType)
-        professorType = UserType("PROFESSOR", True, True)
-        db.session.add(professorType)
-        db.session.commit()
+        studentType = UserType(STUDENT_TYPE, False, True)
+        professorType = UserType(PROFESSOR_TYPE, True, True)
+        db_add(studentType, professorType)
 
         # Create the users
         studentUser = User("Test", "Student", "student@gatech.edu", studentType.id)
-        db.session.add(studentUser)
         professorUser = User("Test", "Professor", "professor@gatech.edu", professorType.id)
-        db.session.add(professorUser)
-        db.session.commit()
+        db_add(studentUser, professorUser)
 
         # Create the authentication for each user
         studentAuth = Authentication(studentUser.id, "password")
-        db.session.add(studentAuth)
         professorAuth = Authentication(professorUser.id, "password")
-        db.session.add(professorAuth)
-        db.session.commit()
+        db_add(studentAuth, professorAuth)
 
         # Create the classes for the test professor
-        class101 = Class('CS 101', 'CS 101 with Test Professor', professorUser.id)
-        db.session.add(class101)
-        class102 = Class('CS 102', 'CS 102 with Test Professor', professorUser.id)
-        db.session.add(class102)
-        class103 = Class('CS 103', 'CS 103 with Test Professor', professorUser.id)
-        db.session.add(class103)
-        class201 = Class('CS 201', 'CS 201 with Test Professor', professorUser.id)
-        db.session.add(class201)
-        db.session.commit()
+        cs101 = Class('CS 101', 'CS 101 with Test Professor', professorUser.id)
+        cs102 = Class('CS 102', 'CS 102 with Test Professor', professorUser.id)
+        cs103 = Class('CS 103', 'CS 103 with Test Professor', professorUser.id)
+        cs201 = Class('CS 201', 'CS 201 with Test Professor', professorUser.id)
+        db_add(cs101, cs102, cs103, cs201)
 
         # Add the test student to CS 101 and CS 102
-        cs101Member = ClassMember(studentUser.id, class101.id)
-        db.session.add(cs101Member)
-        cs102Member = ClassMember(studentUser.id, class102.id)
-        db.session.add(cs102Member)
-        db.session.commit()
+        cs101Member = ClassMember(studentUser.id, cs101.id)
+        cs102Member = ClassMember(studentUser.id, cs102.id)
+        db_add(cs101Member, cs102Member)
+
+        # Create a lecture for CS 101 and CS 102 and simulate voting up for pace and depth
+        # Student is aleady a member of both classes, so he/she should be able to join lectures
+        cs101Lecture = Lecture(cs101.id)
+        cs102Lecture = Lecture(cs102.id)
+        db_add(cs101Lecture, cs102Lecture)
+
+        # Create gauge for student for CS 101, and simulate saying the lecture is too fast and vague
+        cs101Gauge = Gauge(cs101.id, studentUser.id)
+        cs101Gauge.depth = 1
+        cs101Gauge.pace = 1
+        # Create gauge for student for CS 102, and simulate saying the lecture is too slow and detailed
+        cs102Gauge = Gauge(cs102.id, studentUser.id)
+        cs102Gauge.depth = -1
+        cs102Gauge.pace = -1
+        db_add(cs101Gauge, cs102Gauge)
+
+        # Update lecture properties for CS 101 and CS 102
+        cs101Lecture.is_live = True
+        cs101Lecture.pace_total = 1
+        cs101Lecture.depth_total = 1
+        cs101Lecture.num_students = 1
+        cs102Lecture.is_live = True
+        cs102Lecture.pace_total = -1
+        cs102Lecture.depth_total = -1
+        cs102Lecture.num_students = 1
+        db_add(cs101Lecture, cs102Lecture)
+
+        # Create anonymous question on behalf of student for both classes
+        q1 = "Is Howey L1 the correct room for CS 101?"
+        cs101AnonQuestion = AnonymousQuestion(cs101.id, q1, studentUser.id)
+        q2 = "Is Howey L2 the correct room for CS 102?"
+        cs102AnonQuestion = AnonymousQuestion(cs102.id, q2, studentUser.id)
+        db_add(cs101AnonQuestion, cs102AnonQuestion)
 
 from ClassMoodApp.Controllers import *
