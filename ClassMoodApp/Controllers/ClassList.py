4cc3418a25545bd06592f4e85f10e9abd0aaefce
Neil Goel <Neil@res388d-128-61-90-195.res.gatech.edu>
2016-12-05 20:42:12 -0500

    added comments to the controllers

--- ClassMoodApp/Controllers/ClassList.py
+++ ClassMoodApp/Controllers/ClassList.py
@@ -6,7 +6,7 @@ import json
 login_page = 'authentication/login.html'
 main_page = 'classList.html'
 
-
+# Returns a template for the class list page
 @app.route('/classList', methods=['GET'])
 def classList():
     authUser = API.get_authentication()
@@ -14,6 +14,7 @@ def classList():
         return render_template(login_page, error='You are not logged in')
     return render_template(main_page, username=API.get_authentication().first_name, user_id=API.get_authentication().id)
 
+# Returns the list of classes for the current user
 @app.route('/getClassList', methods=['GET'])
 def getClassList():
     authUser = API.get_authentication()
@@ -34,6 +35,7 @@ def getClassList():
             class_info.append({'className': each_class, 'id' : class_id, 'liveLectureId': live_lecture_id, 'is_live': False})
     return jsonify(results=class_info)
 
+# Returns if the current user is a student
 @app.route('/is_student', methods=['GET'])
 def is_student():
     authUser = API.get_authentication()
@@ -41,6 +43,7 @@ def is_student():
         return render_template(login_page, error='You are not logged in')
     return jsonify(results=API.is_student(authUser.id))
 
+# Creates a class with the given class name
 @app.route('/createClass/<className>', methods=['GET','POST'])
 def createClass(className):
     authUser = API.get_authentication()
@@ -48,6 +51,7 @@ def createClass(className):
         return render_template(login_page, error='You are not logged in')
     return json.dumps(API.create_prof_class(className, authUser.id))
 
+# Adds a class for the student
 @app.route('/set_student_classes/<class_name>/<int:student_id>', methods = ['GET', 'POST'])
 def set_student_classes(class_name, student_id):
     authUser = API.get_authentication()
@@ -55,6 +59,7 @@ def set_student_classes(class_name, student_id):
         return render_template('authentication/login.html', error='You are not logged in')
     return json.dumps(API.set_student_class(class_name, student_id))
 
+# Deletes a class and all associations with it
 @app.route('/delete/class/<int:class_id>', methods = ['GET', 'POST'])
 def delete_class(class_id):
     authUser = API.get_authentication()
@@ -62,6 +67,7 @@ def delete_class(class_id):
         return render_template('authentication/login.html', error='You are not logged in')
     return jsonify(results=API.delete_class(class_id))
 
+# Removes a class from a student
 @app.route('/delete/class_student/<int:student_id>/<int:class_id>', methods = ['GET', 'POST'])
 def delete_class_student(student_id, class_id):
     authUser = API.get_authentication()
